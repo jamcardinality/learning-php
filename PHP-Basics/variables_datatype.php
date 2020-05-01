d9b19271ae852d36ee92615768d25d44ee212e3c
jamcardinality <jamescarlcardinal@gmail.com>
2020-05-02 02:39:46 +0800

    Build: Add Variables and Data Types

---
+++ PHP-Basics/variables_datatype.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Variables and Data Type</title>
+</head>
+
+<body bgcolor="#f3f3f3">
+  <center>
+    <h1>
+        <?php 
+          $first_name = "Jam"; //String
+          $age = 21; //Number
+          $price = 19.95; //Float
+          $alive = TRUE; //Boolean
+
+          echo $alive;
+        ?>
+    </h1>
+  </center>
+    
+</body>
+</html>
