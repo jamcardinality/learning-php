461f3fff91a35acafc671ff5d609f34a6e67af10
jamcardinality <jamescarlcardinal@gmail.com>
2020-05-02 02:49:48 +0800

    Build: Add Math Operators

---
+++ PHP-Basics/math_operators.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Math Operators</title>
+</head>
+
+<body bgcolor="#f3f3f3">
+  <center>
+    <h1>
+        <?php 
+          //Math Operators 
+          // + Addition, - Subtraction, * Multiplication, / Division, % Modulus, ** Exponent 
+
+          $num1 = 10;
+          $num2 = 3;
+
+          echo $num1 % $num2;
+        ?>
+    </h1>
+  </center>
+    
+</body>
+</html>
