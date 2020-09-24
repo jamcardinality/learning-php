7ea45d8c19233771c2f9c121024aa36b25d8dc5d
jamcardinality <jamescarlcardinal@gmail.com>
2020-09-25 02:14:49 +0800

    Build: Add Increment and Decrement Operators

---
+++ PHP-Basics/Increment_Decrement_Operators.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Assignment Operators</title>
+</head>
+
+<body bgcolor="#f3f3f3">
+  <center>
+    <h1>
+        <?php 
+          //Increment and Decrement Operators 
+          // ++$x $x++ 
+          // --$x $x--
+          // Pre-Increment means it increments by 1 and then return the variable
+          // Post-Increment means it returns the variable and then it increments it by 1
+
+          $num1 = 15;
+            //echo $num1 = $num1 + 1; or ++$num1
+          echo $num1++;
+          echo "<br>";
+          echo $num1;
+          
+        ?>
+    </h1>
+  </center>
+    
+</body>
+</html>
