6916a111ae9bd7b7687c5d76c66322fc0365a039
jamcardinality <jamescarlcardinal@gmail.com>
2020-09-25 01:49:48 +0800

    Build: Add Comparison Operators

---
+++ PHP-Basics/comparison_operators.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Assignment Operators</title>
+</head>
+
+<body bgcolor="#f3f3f3">
+  <center>
+    <h1>
+        <?php 
+          //Comparison Operators 
+          // == equal, === Identical, != Not Equal, <> Not Equal, !== Not Identical
+          // > Greater Than, < Less Than, >= Greater Than or Equal, <= Less Than or Equal
+          // Identical means the datatypes that you'll use are the same
+        
+          $num1 = "10";
+          $num2 = 10; 
+          
+          // This is not identical
+        ?>
+    </h1>
+  </center>
+    
+</body>
+</html>
